2e925f1dcd9554772037fae4bf965ba4e4b1ec08
fabielone <f4b13l90@gmail.com>
2022-02-18 11:29:27 -0800

    jacobi wip

--- c/jacobi.c
+++ c/jacobi.c
@@ -16,9 +16,11 @@ int main()
     printf("%d", n);
     float xij[n][n + 1];
 
+    // ingresar valores de la matriz
+
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n+1; j++)
+        for (int j = 0; j < n + 1; j++)
         {
             if (j == n)
             {
---
+++ pEst/actividad 2/FRG_ACT2_1.c
@@ -0,0 +1,13 @@
+// Fabiel Ramirez Garcia 368036
+// 11-Feb-20222 -----
+// Leer 2 numeros, realizar la suma y desplegar la suma de los 2 numeros.
+// FRG_ACT2_1_PE.C
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world");
+}
