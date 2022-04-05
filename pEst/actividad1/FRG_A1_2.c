8e62e165eff083050da0a4bd4000993841c01495
fabielone <f4b13l90@gmail.com>
2022-04-05 02:35:22 -0700

    some changes

--- pEst/actividad1/FRG_A1_2.c
+++ pEst/actividad1/FRG_A1_2.c
@@ -3,3 +3,28 @@
 // Programa en C que lea un número entero, y desplegar si el número es “PAR” o “IMPAR”
 //
 // FRG_ACT1_2.c
+
+#include <stdio.h>
+#include "./../../libraries/getch.h"
+#include <stdlib.h>
+
+int main(void)
+{
+  char c;
+  system("clear");
+  printf("Cual es su genero 1.-Masculino 2.-Femenino\n");
+  c = getche();
+
+  if (c == 49)
+  {
+    printf("\nMASCULINO\n");
+  }
+  else if (c == 50)
+  {
+    printf("\nFEMENINO\n");
+  }
+  else
+  {
+    printf("\nno entendi\n");
+  }
+}
