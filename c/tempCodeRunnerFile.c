884f85626df52c197a2fb63c97612c53b6a67713
fabielone <fabielone@gmail.com>
2022-06-10 21:28:38 -0700

    updates

--- c/tempCodeRunnerFile.c
+++ c/tempCodeRunnerFile.c
@@ -1,37 +1,5 @@
+#include <math.h>
 #include <stdio.h>
-
-int main()
-{
-    int array[100], n, c, d, swap;
-
-    printf("Ingrese tamano de arreglo: ");
-    scanf("%d", &n);
-    printf("Ingrese un numero:\n");
-
-    for (c = 0; c < n; c++)
-
-        scanf(" %i", &array[c]);
-
-    for (c = 0; c < n - 1; c++)
-    {
-        for (d = 0; d < n - c - 1; d++)
-        {
-            if (array[d] > array[d + 1])
-            {
-                swap = array[d];
-                array[d] = array[d + 1];
-                array[d + 1] = swap;
-            }
-        }
-    }
-
-    printf("Lista ordenada \n");
-
-    for (c = 0; c < n; c++)
-        printf("%d,", array[c]);
-
-    return 0;
-}
-
-
-
+int main() {
+    
+} 
